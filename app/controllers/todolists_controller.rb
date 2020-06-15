8c3adc86f97ea5e96f08e385e9efded7988dc303
Tatsuro Muto <tomuto0221@gmail.com>
2020-06-15 15:12:50 +0900

    first commit

---
+++ app/controllers/todolists_controller.rb
@@ -0,0 +1,46 @@
+class TodolistsController < ApplicationController
+  def new
+  	#Viewへ渡すためのインスタンス係数に空のモデルオブジェクトを生成する。
+  	 @list=List.new
+  end
+
+  #以下を追加
+  def create
+  	   #ストロングパラメーターを使用
+  	   list=List.new(list_params)
+  	   #DBへ保存する
+  	   list.save
+  	   #トップ画面へリダイレクト
+  	   redirect_to todolist_path(list.id)#詳細画面へリダイレクト
+  end
+
+  def index
+  	@lists =List.all
+  end
+
+  def show
+      @list =List.find(params[:id])
+  end
+
+  def edit
+  	  @list =List.find(params[:id])
+  end
+
+  def updete
+  	  list =List.find(params[:id])
+  	  list.update(list_params)
+  	  redirect_to todolist_path(list.id)
+  end
+  def destroy
+  	  list =List.find(params[:id])#データ(レコード)を1件取得
+  	  list.destroy#データ(レコード)を削除
+  	  redirect_to todolists_path#List一覧画面へリダイレクト
+  end
+
+  private
+
+  def list_params
+  	params.require(:list).permit(:title,:body,:image)
+  end
+
+end
---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = '0f588296400940f6cbd5d71171f9f5479f6d216c6e78ff50d480a217dbf12be1cc1e9b029d3641a858a70675a60fcbc12ef3ba23ec5b94ef8812519dae92c541'
